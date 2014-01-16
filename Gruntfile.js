2b967572bd0d0ceedd5cd9a7e0d38cc28631b50d
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-01-16 11:40:21 -0500

    added live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,9 @@ module.exports = function(grunt) {
         },
 
         watch: {
+            options: {
+                livereload: true
+            },
             scripts: {
                 files: ['js/*.js'],
                 tasks: ['uglify'],
